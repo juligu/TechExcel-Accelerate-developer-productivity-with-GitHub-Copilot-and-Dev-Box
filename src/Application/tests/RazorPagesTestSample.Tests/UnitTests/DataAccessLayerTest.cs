f9e57d0a1a5afad3d38304f7027bdac119c33a7d
Julio Guzman <juligu@microsoft.com>
2024-09-13 14:57:40 -0400

    Test

--- src/Application/tests/RazorPagesTestSample.Tests/UnitTests/DataAccessLayerTest.cs
+++ src/Application/tests/RazorPagesTestSample.Tests/UnitTests/DataAccessLayerTest.cs
@@ -44,7 +44,7 @@ namespace RazorPagesTestSample.Tests.UnitTests
 
                 // Assert
                 var actualMessage = await db.FindAsync<Message>(recId);
-                Assert.Equal("Test Data", actualMessage);
+                Assert.Equal(new Message() { Id = recId, Text = "Test" }, actualMessage);
             }
         }
 
